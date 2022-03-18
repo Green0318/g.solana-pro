bc0438fe8364b15d56cc7b76128b7b7e8975cd6c
greenpeace318 <greenpeace0318@gmail.com>
2022-03-18 16:43:24 -0600

    Cargo format

--- programs/track_upload/src/lib.rs
+++ programs/track_upload/src/lib.rs
@@ -6,8 +6,12 @@ declare_id!("Fg6PaFpoGXkYsidMpWTK6W2BeZ7FEfcYkg476zPFsLnS");
 pub mod track_upload {
     use super::*;
 
-    pub fn initialize(ctx: Context<Initialize>, cid: String, 
-        artist: String, title: String) -> Result<()> {
+    pub fn initialize(
+        ctx: Context<Initialize>,
+        cid: String,
+        artist: String,
+        title: String,
+    ) -> Result<()> {
         let track = &mut ctx.accounts.track;
         // Validate Lengths
         require!(cid.chars().count() <= 47, TrackError::InvalidCID);
@@ -22,23 +26,35 @@ pub mod track_upload {
         Ok(())
     }
 
-    // Would be prefered to take Option<String> as args but 
+    // Would be prefered to take Option<String> as args but
     // running into issues with serialization
-    pub fn update(ctx: Context<UpdateTrack>, cid: String, 
-        artist: String, title: String) -> Result<()> {
+    pub fn update(
+        ctx: Context<UpdateTrack>,
+        cid: String,
+        artist: String,
+        title: String,
+    ) -> Result<()> {
         let track = &mut ctx.accounts.track;
-        require!(ctx.accounts.signer.key() == track.signer, TrackError::UnauthorizedUser);
+        require!(
+            ctx.accounts.signer.key() == track.signer,
+            TrackError::UnauthorizedUser
+        );
         // Validate Lengths
         require!(cid.chars().count() <= 47, TrackError::InvalidCID);
         require!(artist.chars().count() <= 32, TrackError::TrackTooLong);
         require!(title.chars().count() <= 32, TrackError::ArtistTooLong);
 
-        if cid.chars().count() > 0 {track.cid = cid.as_bytes().to_vec()};
-        if artist.chars().count() > 0 {track.artist = artist.as_bytes().to_vec()};  
-        if title.chars().count() > 0 {track.track_title = title.as_bytes().to_vec()};      
-        Ok(())  
+        if cid.chars().count() > 0 {
+            track.cid = cid.as_bytes().to_vec()
+        };
+        if artist.chars().count() > 0 {
+            track.artist = artist.as_bytes().to_vec()
+        };
+        if title.chars().count() > 0 {
+            track.track_title = title.as_bytes().to_vec()
+        };
+        Ok(())
     }
-
 }
 
 #[derive(Accounts)]
@@ -55,19 +71,17 @@ pub struct UpdateTrack<'info> {
     #[account(mut, has_one = signer)]
     pub track: Account<'info, Track>,
     #[account(mut)]
-    pub signer: Signer<'info>
+    pub signer: Signer<'info>,
 }
-
 
 #[account]
 #[derive(Default)]
-pub struct Track  {
-    pub cid: Vec<u8>, //47
-    pub artist: Vec<u8>, //32
+pub struct Track {
+    pub cid: Vec<u8>,         //47
+    pub artist: Vec<u8>,      //32
     pub track_title: Vec<u8>, //32
-    pub signer: Pubkey //64
+    pub signer: Pubkey,       //64
 }
-
 
 #[error_code]
 pub enum TrackError {
