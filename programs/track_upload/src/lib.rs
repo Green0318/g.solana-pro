0a5463c00fde2b93a8917c9298110dbf6d7c8583
greenpeace318 <greenpeace0318@gmail.com>
2022-03-23 00:31:51 -0600

    updated programid

--- programs/track_upload/src/lib.rs
+++ programs/track_upload/src/lib.rs
@@ -1,6 +1,6 @@
 use anchor_lang::prelude::*;
 
-declare_id!("Fg6PaFpoGXkYsidMpWTK6W2BeZ7FEfcYkg476zPFsLnS");
+declare_id!("Bou2Yfi3uVrHi1FxHuHcgYFa5Q5M4bSoXK3NHpZy8Zd6");
 
 #[program]
 pub mod track_upload {
